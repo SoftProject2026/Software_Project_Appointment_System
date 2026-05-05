1ee5f2bf6eef7a4b77caf3853424fdde1f00d58e
sarasawalha1 <sarasawalha74@gmail.com>
2026-05-05 20:07:39 +0300

    Fix JUnit imports

--- src/test/java/test/testAppointmentService.java
+++ src/test/java/test/testAppointmentService.java
@@ -1,6 +1,5 @@
 package test;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
