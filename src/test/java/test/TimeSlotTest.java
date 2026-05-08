40e17280d8ff03df11ed58ed938d657d707940e1
Masar Jabr <s12323291@stu.najah.edu>
2026-05-09 00:55:29 +0200

    test

---
+++ src/test/java/test/TimeSlotTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import com.appointmentsystem.domain.models.TimeSlot;
+
+public class TimeSlotTest {
+
+    @Test
+    void testCreateTimeSlot() {
+
+        LocalDateTime time =
+                LocalDateTime.now().plusDays(1);
+
+        TimeSlot slot = new TimeSlot(time);
+
+        assertTrue(slot.isAvailable());
+        assertEquals(time, slot.getStartTime());
+    }
+
+    @Test
+    void testPastTimeSlot() {
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new TimeSlot(
+                LocalDateTime.now().minusDays(1)
+            )
+        );
+    }
+
+    @Test
+    void testSetAvailable() {
+
+        TimeSlot slot = new TimeSlot(
+                LocalDateTime.now().plusHours(1)
+        );
+
+        slot.setAvailable(false);
+
+        assertFalse(slot.isAvailable());
+    }
+}
